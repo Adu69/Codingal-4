3a7c2690f99649365c237102593dda1943fecb6b
Adu69 <83687446+Adu69@users.noreply.github.com>
2026-03-29 12:55:05 +0530

    Add files via upload

---
+++ number of bits.py
@@ -0,0 +1,8 @@
+def numberOfBits(n):
+    count = 0
+    while(n):
+        count += 1
+        n >>= 1
+    return count
+number = int(input("Enter a number: "))
+print("Number of bits required to represent", number, "is", numberOfBits(number))
